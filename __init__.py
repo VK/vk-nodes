3379cbb5118936b8cb75ddfa7ceef0b6581e67a1
VK <viktor@krueckl.de>
2025-04-24 19:11:03 +0200

    fix import problem

--- __init__.py
+++ __init__.py
@@ -2,4 +2,19 @@
 from .vk_tiled_render import TiledRenderNode, PrepareJobs
 from .vk_tiled_setup import TiledSetupNode, TiledCropNode
 
-UE_VERSION = "1.0.1"
+
+from .vk_tiled_render import NODE_CLASS_MAPPINGS as NODE_CLASS_MAPPINGS
+from .vk_tiled_render import NODE_DISPLAY_NAME_MAPPINGS as NODE_DISPLAY_NAME_MAPPINGS
+
+from .vk_tiled_setup import NODE_CLASS_MAPPINGS as NODE_CLASS_MAPPINGS_A
+from .vk_tiled_setup import NODE_DISPLAY_NAME_MAPPINGS as NODE_DISPLAY_NAME_MAPPINGS_A
+
+NODE_CLASS_MAPPINGS.update(NODE_CLASS_MAPPINGS_A)
+NODE_DISPLAY_NAME_MAPPINGS.update(NODE_DISPLAY_NAME_MAPPINGS_A)
+
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
+
+
+
+UE_VERSION = "1.0.2"
